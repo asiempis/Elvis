9bc3caf6465a90aae441012b96a0f47e91898090
Aristotelis Siempis <asiempis@gmail.com>
2015-04-09 14:14:42 +0300

    Add Google form to modal
    Add google form to modal and submit via ajax to google spreadsheet

--- js/elvis.js
+++ js/elvis.js
@@ -36,6 +36,7 @@ $(document).idle({
 
 $( "button.md-close" ).click(function() {
  $("#modal-3").removeClass("md-show");
+ $("#modal-gform").removeClass("md-show");
 });
 
 
@@ -55,8 +56,38 @@ addEvent(document, "mouseout", function(e) {
 	var elvisHasLeft = $.cookie('elvisHasLeft');
 	if(elvisHasLeft!=1){
 		if (!from || from.nodeName == "HTML") {
-			$("#modal-3").addClass("md-show");
+			$("#modal-gform").addClass("md-show");
 			$.cookie('elvisHasLeft', '1', { expires: 1 });
 		}
 	}
 });
+
+
+function postContactToGoogle(){
+        var answer = $('[name="elvisSong"]:checked').val();
+        if(answer == "Other"){
+        	answer ="__other_option__";
+        	var otherAnswer = $('[name="elvisSong.other_option_response"]').val();
+        }
+        if (answer !== "") {
+            $.ajax({
+                url: "https://docs.google.com/forms/d/<form-id>/formResponse",
+                data: {"entry.1" : answer, "entry.2.other_option_response" : otherAnswer},// data: {"entry.215041315" : answer, "entry.3" : email, "entry.4": feed},
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function (){
+                        $("#modal-gform").removeClass("md-show");
+                        $('#elvis-gform')[0].reset();
+                    },
+                    200: function (){
+                       $("#modal-gform").removeClass("md-show");
+                        $('#elvis-gform')[0].reset();
+                    }
+                }
+            });
+        }
+        else {
+            //Error message
+        }
+    }
